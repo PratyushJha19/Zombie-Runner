f1f95dec72a1342322d7da8a12fe89228babbfbf
PratyushJha19 <jhapratyush2006@gmail.com>
2023-03-29 15:22:01 +0530

    Error Correction

--- Assets/CursorsAndCrosshairs/CursorChanger.cs
+++ Assets/CursorsAndCrosshairs/CursorChanger.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CursorChanger : MonoBehaviour {
+public class CursorChanger2 : MonoBehaviour {
 
     public Texture2D cursor;
 
